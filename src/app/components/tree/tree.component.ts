69e5fdc3f38b1049daf87b0d99db62d4f12c17f1
lironlahav <lironl397@gmail.com>
2021-12-11 11:30:33 +0200

    modifications

--- src/app/components/tree/tree.component.ts
+++ src/app/components/tree/tree.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ApiService} from "../../services/api.service";
 import {UtilsService} from "../../services/utils.service";
 import {faDatabase, faFile, faTable, faColumns} from '@fortawesome/free-solid-svg-icons';
